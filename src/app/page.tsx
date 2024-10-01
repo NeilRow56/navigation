3f789a60b66a36cc430e26fe5ee56cd4c1f0cf24
NeilRow56 <neilrowland56@gmail.com>
2024-10-01 11:22:20 +0100

    Basic Home Page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import { SiteHeader } from "@/components/frontEnd/SiteHeader";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-5xl font-bold">Home page</h1>
-      <Button asChild>
-        <Link href="/dashboard">Dashboard</Link>
-      </Button>
+    <div>
+      <SiteHeader />
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1 className="text-5xl font-bold">Home page</h1>
+        <Button asChild>
+          <Link href="/dashboard">Dashboard</Link>
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ src/components/frontEnd/SiteHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function SiteHeader() {
+  return (
+    <div className="flex w-full bg-gray-100 h-16 items-center container mx-auto px-8 lg:px-24">
+      SiteHeader
+    </div>
+  );
+}
