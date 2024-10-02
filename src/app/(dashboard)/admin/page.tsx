1d3baaebc4c83eb38aec119af405a25c7857aa38
NeilRow56 <neilrowland56@gmail.com>
2024-10-02 15:23:38 +0100

    Completed admin dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,7 @@
+import Announcements from "@/components/users/Announcements";
 import AttendanceChart from "@/components/users/AttendanceChart";
 import CountChart from "@/components/users/CountChart";
+import EventCalendar from "@/components/users/EventCalendar";
 import FinanceChart from "@/components/users/FinanceChart";
 import UserCard from "@/components/users/UserCard";
 import React from "react";
@@ -34,7 +36,10 @@ const AdminPage = () => {
       </div>
 
       {/* Right */}
-      <div className="w-full lg:w-1/3 bg-green-50">Right</div>
+      <div className="w-full flex flex-col gap-8 lg:w-1/3 bg-green-50">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
 };
