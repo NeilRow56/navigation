8f23cc3d1b85f61f7f4080c3388b73c36e4f16ab
NeilRow56 <neilrowland56@gmail.com>
2024-10-13 11:28:24 +0100

    Subject dlete and Form Container added

---
+++ src/components/users/FormContainer.tsx
@@ -0,0 +1,58 @@
+import db from "@/lib/db";
+import FormModal from "./FormModal";
+import { auth } from "@clerk/nextjs/server";
+
+export type FormContainerProps = {
+  table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
+  type: "create" | "update" | "delete";
+  data?: any;
+  id?: number | string;
+};
+
+const FormContainer = async ({ table, type, data, id }: FormContainerProps) => {
+  let relatedData = {};
+
+  const { userId, sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+
+  if (type !== "delete") {
+    switch (table) {
+      case "subject":
+        const subjectTeachers = await db.teacher.findMany({
+          select: { id: true, name: true, surname: true },
+        });
+        relatedData = { teachers: subjectTeachers };
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return (
+    <div className="">
+      <FormModal
+        table={table}
+        type={type}
+        data={data}
+        id={id}
+        relatedData={relatedData}
+      />
+    </div>
+  );
+};
+
+export default FormContainer;
--- src/components/users/FormModal.tsx
+++ src/components/users/FormModal.tsx
@@ -2,7 +2,27 @@
 
 import dynamic from "next/dynamic";
 import Image from "next/image";
-import { Dispatch, SetStateAction, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useFormState } from "react-dom";
+import { toast } from "react-toastify";
+import { FormContainerProps } from "./FormContainer";
+import { deleteSubject } from "@/actions/subject";
+
+type DeleteFunction = typeof deleteSubject;
+
+const deleteActionMap: Record<string, DeleteFunction> = {
+  subject: deleteSubject,
+
+  // Placeholders pour les futures implémentations
+  parent: deleteSubject,
+  lesson: deleteSubject,
+  assignment: deleteSubject,
+  result: deleteSubject,
+  attendance: deleteSubject,
+  event: deleteSubject,
+  announcement: deleteSubject,
+};
 
 // USE LAZY LOADING
 
@@ -19,30 +39,39 @@ const SubjectForm = dynamic(() => import("../forms/SubjectForm"), {
   loading: () => <h1>Loading...</h1>,
 });
 
+// TODO: OTHER FORMS
+
 const forms: {
   [key: string]: (
     setOpen: Dispatch<SetStateAction<boolean>>,
-
     type: "create" | "update",
-
     data?: any,
+    relatedData?: any,
   ) => JSX.Element;
 } = {
-  subject: (setOpen, type, data) => (
-    <SubjectForm type={type} data={data} setOpen={setOpen} />
+  subject: (setOpen, type, data, relatedData) => (
+    <SubjectForm
+      type={type}
+      data={data}
+      setOpen={setOpen}
+      // relatedData={relatedData}
+    />
   ),
-  teacher: (setOpen, type, data) => (
+
+  teacher: (setOpen, type, data, relatedData) => (
     <TeacherForm
       type={type}
       data={data}
       // setOpen={setOpen}
+      // relatedData={relatedData}
     />
   ),
-  student: (setOpen, type, data) => (
+  student: (setOpen, type, data, relatedData) => (
     <StudentForm
       type={type}
       data={data}
       // setOpen={setOpen}
+      // relatedData={relatedData}
     />
   ),
 };
@@ -52,24 +81,8 @@ const FormModal = ({
   type,
   data,
   id,
-}: {
-  table:
-    | "teacher"
-    | "student"
-    | "parent"
-    | "subject"
-    | "class"
-    | "lesson"
-    | "exam"
-    | "assignment"
-    | "result"
-    | "attendance"
-    | "event"
-    | "announcement";
-  type: "create" | "update" | "delete";
-  data?: any;
-  id?: number;
-}) => {
+  relatedData,
+}: FormContainerProps & { relatedData?: any }) => {
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
   const bgColor =
     type === "create"
@@ -81,8 +94,24 @@ const FormModal = ({
   const [open, setOpen] = useState(false);
 
   const Form = () => {
+    const [state, formAction] = useFormState(deleteActionMap[table], {
+      success: false,
+      error: false,
+    });
+
+    const router = useRouter();
+
+    useEffect(() => {
+      if (state.success) {
+        toast(`${table} has been deleted!`);
+        setOpen(false);
+        router.refresh();
+      }
+    }, [state, router]);
+
     return type === "delete" && id ? (
-      <form action="" className="flex flex-col gap-4 p-4">
+      <form action={formAction} className="flex flex-col gap-4 p-4">
+        <input type="text | number" name="id" value={id} hidden />
         <span className="text-center font-medium">
           All data will be lost. Are you sure you want to delete this {table}?
         </span>
@@ -91,7 +120,7 @@ const FormModal = ({
         </button>
       </form>
     ) : type === "create" || type === "update" ? (
-      forms[table](setOpen, type, data)
+      forms[table](setOpen, type, data, relatedData)
     ) : (
       "Form not found!"
     );
